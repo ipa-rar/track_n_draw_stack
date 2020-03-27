49a25ad4037d3b1b4128209499ef67ca03ce43b6
Ragesh <ragesh.ramachandran@ipa.fraunhofer.de>
2020-03-27 20:42:19 +0100

    made changes to ompl_task and made it work on manipulator move_group

--- track_n_draw/script/application.py
+++ track_n_draw/script/application.py
@@ -7,24 +7,69 @@ import rospy
 __REQUIRED_API_VERSION__ = "1"
 __ROBOT_VELOCITY__ = 0.5
 
+
 class TrackNDraw():
     def __init__(self):
         self.r = Robot(__REQUIRED_API_VERSION__)
         home_pos = [1.69, -0.07, 1.41, -0.14, 0.16, -0.63]
         self.r.move(Ptp(goal=home_pos, vel_scale=__ROBOT_VELOCITY__))
-    
+
     def draw_circle(self):
-        pass
+        start_pos = Pose(position=Point(0.10, -0.78, 0.25))
+        center_pos = Pose(position=Point(0, -0.68, 0.25))
+        goal_pos = Pose(position=Point(-0.10, -0.78, 0.25))
+        # planners starting here
+        try:
+            self.r.move(Ptp(goal=start_pos, vel_scale=__ROBOT_VELOCITY__))
+        except RobotMoveFailed:
+            rospy.loginfo("The pose is not unreachable")
 
-    def move_to_center(self):
-        pass
+        try:
+            self.r.move(Circ(goal=goal_pos, center=center_pos, acc_scale=0.4))
+        except RobotMoveFailed:
+            rospy.loginfo("The pose is unreachable")
 
-    def task(self):
-        pass
+    def move_robot(self):
+        joint_goal = [1.1450463005300113, 1.3449728449134062, 0.3303570566119351, -
+                      1.2896732177397627, -1.2425725076038912, -1.762039518051274]
+        cartesian_goal = Pose(position=Point(0.540, -0.283, 0.682),
+                              orientation=Quaternion(-0.000, 0.726, 0.000, 0.688))
+        self.r.move(Ptp(goal=joint_goal, vel_scale=__ROBOT_VELOCITY__))
+        self.r.move(Ptp(goal=cartesian_goal, vel_scale=__ROBOT_VELOCITY__))
+
+    def pick_and_place(self):
+        self.r.move(Lin(goal=Pose(position=Point(0, 0, 0.1)),
+                        reference_frame="prbt_tcp", vel_scale=0.2))
+        rospy.loginfo("Open/Close the gripper")  # log
+        rospy.sleep(0.2)
+        self.r.move(Lin(goal=Pose(position=Point(0, 0, -0.1)),
+                        reference_frame="prbt_tcp", vel_scale=0.2))
+
+    def start_program(self):
+        # positions declared
+        start_pose = [1.49, -0.54, 1.09, 0.05, 0.91, -1.67]
+        pick_pos = Pose(position=Point(-0.0060056, -0.815452, 0.150537),
+                        orientation=Quaternion(0.998554, 0.0525097, 0.00671931, 0.00937636))
+        work_pos = Pose(position=Point(-0.018218, -0.90926, 0.343775),
+                        orientation=Quaternion(0.998554, 0.0525097, 0.00671931, 0.00937636))
+        place_pos = Pose(position=Point(-0.0458218, -0.890926, 0.223775),
+                         orientation=Quaternion(0.998554, 0.0525097, 0.00671931, 0.00937636))
+        # default start position
+        self.r.move(Ptp(goal=start_pose, vel_scale=__ROBOT_VELOCITY__))
+        # Logic loops here
+        while(True):
+            self.r.move(Ptp(goal=pick_pos, vel_scale=__ROBOT_VELOCITY__))
+            self.pick_and_place()
+            rospy.sleep(1)
+            self.r.move(Ptp(goal=place_pos, vel_scale=__ROBOT_VELOCITY__))
+            self.pick_and_place()
+            rospy.sleep(1)
+
 
 def main():
     rospy.init_node('track_n_draw_node')
     track_n_draw = TrackNDraw()
+    track_n_draw.start_program()
 
 
 if __name__ == "__main__":
--- track_n_draw/script/ompl_application.py
+++ track_n_draw/script/ompl_application.py
@@ -10,52 +10,45 @@ from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 # Parameters
 
+class OmplTask():
+    def __init__(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        group_name = "manipulator"
+        self.move_group = moveit_commander.MoveGroupCommander(group_name)
+
+    def display_details(self):
+        planning_frame = self.move_group.get_planning_frame()
+        print "============ Planning frame: %s" % planning_frame
+        eef_link = self.move_group.get_end_effector_link()
+        print "============ End effector link: %s" % eef_link
+        group_names = self.robot.get_group_names()
+        print "============ Available Planning Groups:", self.robot.get_group_names()
+        print "============ Printing robot state"
+        print(self.robot.get_current_state())
+        print ""
+
+    def target_pos(self):
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.position.x = -0.049
+        pose_goal.position.y = -0.37
+        pose_goal.position.z = 0.7
+        self.move_group.set_pose_target(pose_goal)
+
+        plan = self.move_group.go(wait=True)
+
+    def cleanup(self):
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+
+
 def main():
-    moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('track_n_draw_node')
-    robot = moveit_commander.RobotCommander()
-    scene = moveit_commander.PlanningSceneInterface()
-    group_name = "manipulator_tcp"
-    move_group = moveit_commander.MoveGroupCommander(group_name)
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                moveit_msgs.msg.DisplayTrajectory,
-                                                queue_size=20)
-
-    # We can get the name of the reference frame for this robot:
-    planning_frame = move_group.get_planning_frame()
-    print "============ Planning frame: %s" % planning_frame
-
-    # We can also print the name of the end-effector link for this group:
-    eef_link = move_group.get_end_effector_link()
-    print "============ End effector link: %s" % eef_link
-
-    # We can get a list of all the groups in the robot:
-    group_names = robot.get_group_names()
-    print "============ Available Planning Groups:", robot.get_group_names()
-
-    # Sometimes for debugging it is useful to print the entire state of the
-    # robot:
-    print "============ Printing robot state"
-    print (robot.get_current_state())
-    print ""
-
-    # We can get the joint values from the group and adjust some of the values:
-    pose_goal = geometry_msgs.msg.Pose()
-    print "current pose"
-    print pose_goal
-    pose_goal.position.x = 0.012
-    pose_goal.position.y = -0.45
-    pose_goal.position.z = 0.5
-
-    move_group.set_pose_target(pose_goal)
-    
-
-    plan = move_group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    move_group.stop()
-    # It is always good to clear your targets after planning with poses.
-    # Note: there is no equivalent function for clear_joint_value_targets()
-    move_group.clear_pose_targets()
+    planer = OmplTask()
+    planer.display_details()
+    planer.target_pos()
+    planer.cleanup()
 
 
 if __name__ == "__main__":
